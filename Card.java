b15356af603507d7b97474bc6c897c6a1a210ca0
Dylan283 <mutantrinzler@gmail.com>
2025-09-25 17:39:17 +0000

    Add ons

--- Card.java
+++ Card.java
@@ -1,44 +1,69 @@
+//Shane
+//Tristen
+//Dylan
+//9/23/25
+//Arraylist card game with OOP
+
 public class Card {
-    //data variables
+    //Shane: data variables
     private String cardSuit;
-
     private String cardName;
-
     private int cardValue;
-
     private String cardPicture;
-    
+
+    //Shane: default constructor
     public Card () {
-        cardSuit = "";
-        cardName = "";
-        cardValue = 0;
-        cardPicture = "";
+        cardSuit = "";// Shane: default suit
+        cardName = "";// Shane: default name
+        cardValue = 0;// Shane: default value
+        cardPicture = "";// Shane: default picture
     }
-    
-    //constructor
+
+    //Shane: parameterized constructor
     public Card (String cardSuit, String cardName, int cardValue, String cardPicture) {
-        this.cardSuit = cardSuit;
-        this.cardName = cardName;
-        this.cardValue = cardValue;
-        this.cardPicture = cardPicture;
+        this.cardSuit = cardSuit;// Shane: default suit
+        this.cardName = cardName;// Shane: default name
+        this.cardValue = cardValue;// Shane: default value
+        this.cardPicture = cardPicture;// Shane: default picture
     }
 
-        
-
-    
+    //Shane: Accessors (getters)
     public String getCardSuit() {
-        return cardSuit;
+        return cardSuit;// Shane: return cards suit
     }
 
     public String getCardName() {
-        return cardName;
+        return cardName;// Shane: return cards name
     }
 
     public int getCardValue() {
-        return cardValue;
+        return cardValue;// Shane: return cards value
     }
 
     public String getCardPicture() {
-        return cardPicture;
+        return cardPicture;// Shane: return cards picture
+    }
+
+    //Shane: Mutators (setters)
+    public void setCardSuit(String cardSuit) {
+        this.cardSuit = cardSuit;// Shane: set cards suit
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;// Shane: set cards name
+    }
+
+    public void setCardValue(int cardValue) {
+        this.cardValue = cardValue;// Shane: set cards value
+    }
+
+    public void setCardPicture(String cardPicture) {
+        this.cardPicture = cardPicture;// Shane: set cards picture
+    }
+
+    //Shane: toString method
+    @Override
+    public String toString() {
+        return "Card: Suit = " + cardSuit + ", Name = " + cardName + ", Value = " + cardValue + ", Picture = " + cardPicture;
     }
 }
